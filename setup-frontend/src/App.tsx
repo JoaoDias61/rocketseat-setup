97a39874216db43dc24578e1c7275cbc001a18db
JoaoDias61 <jpdias641@gmail.com>
2023-01-29 17:20:01 -0300

    first commit

---
+++ setup-frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Habit } from './components/Habit';
+import './styles/global.css'
+
+interface AppProps {}
+
+export function App(props: AppProps) {
+  return (
+    <Habit />
+  );
+}
---
+++ setup-frontend/src/components/Habit.tsx
@@ -0,0 +1,7 @@
+interface HabitProps {}
+
+export function Habit(props: HabitProps) {
+    return (
+        <div className="bg-zinc-900 w-10 h-18 text-white rounded m-2">habit</div>
+    );
+}
